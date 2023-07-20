cc8f850bdd70f629c7e12a9962b8c6b79462df79
Bünyamin Ilhan <ilhan.buenyamin@gmail.com>
2023-07-20 08:55:23 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Todo';
+  description = '';
+
+
+  constructor(private firestore: Firestore) {
+
+  }
+
+  addUser() {
+
+  }
+
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'todo-bi89',
+    appId: '1:754513565239:web:75054a4a7a3a5df1ff51b2',
+    storageBucket: 'todo-bi89.appspot.com',
+    apiKey: 'AIzaSyBje0LONKklJz22wWNLTvQ6P4ZxeppabcY',
+    authDomain: 'todo-bi89.firebaseapp.com',
+    messagingSenderId: '754513565239',
+    measurementId: 'G-4941ZD7NBN',
+  },
+};
